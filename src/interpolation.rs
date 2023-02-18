5d3c8fa470bff194194a3a46ade0ae5bafcec2b5
kuviman <kuviman@gmail.com>
2023-02-18 10:41:24 +0400

    Dynamic interpolation time

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -6,6 +6,7 @@ pub struct Interpolated<T> {
     c: T,
     d: T,
     t: f32,
+    interpolation_time: f32,
 }
 
 // f(t) = A * t^3 + B * t^2 + C * t + D
@@ -40,43 +41,55 @@ impl Zero for vec3<f32> {
     const ZERO: Self = vec3::ZERO;
 }
 
-const INTERPOLATION_TIME: f32 = 0.5;
+const MIN_ITERPOLATION_TIME: f32 = 0.05;
 
 impl<T: Mul<f32, Output = T> + Add<Output = T> + Sub<Output = T> + Copy + Zero> Interpolated<T> {
     pub fn new(p: T, v: T) -> Self {
+        let interpolation_time = MIN_ITERPOLATION_TIME;
         Self {
             a: T::ZERO,
             b: T::ZERO,
-            c: v * INTERPOLATION_TIME,
+            c: v * interpolation_time,
             d: p,
             t: 0.0,
+            interpolation_time,
         }
     }
     pub fn teleport(&mut self, p: T, v: T) {
         self.a = T::ZERO;
         self.b = T::ZERO;
-        self.c = v * INTERPOLATION_TIME;
+        self.c = v * self.interpolation_time;
         self.d = p;
         self.t = 0.0;
     }
     pub fn server_update(&mut self, p2: T, v2: T) {
-        let p2 = p2 + v2 * INTERPOLATION_TIME;
         let p1 = self.get();
         let v1 = self.get_derivative();
-        self.d = p1;
-        self.c = v1 * INTERPOLATION_TIME;
-        self.b = p2 * 3.0 - self.c * 2.0 - self.d * 3.0 - v2 * INTERPOLATION_TIME;
-        self.a = p2 - self.b - self.c - self.d;
-        self.t = 0.0;
+        let interpolation_time = (self.t * 1.5).max(MIN_ITERPOLATION_TIME);
+        let p2 = p2 + v2 * interpolation_time;
+        let d = p1;
+        let c = v1 * interpolation_time;
+        let b = p2 * 3.0 - c * 2.0 - d * 3.0 - v2 * interpolation_time;
+        let a = p2 - b - c - d;
+        *self = Self {
+            a,
+            b,
+            c,
+            d,
+            t: 0.0,
+            interpolation_time,
+        };
     }
     pub fn update(&mut self, delta_time: f32) {
-        self.t = (self.t + delta_time / INTERPOLATION_TIME).min(1.0);
+        self.t += delta_time;
     }
     pub fn get(&self) -> T {
-        self.a * self.t.powi(3) + self.b * self.t.sqr() + self.c * self.t + self.d
+        let t = (self.t / self.interpolation_time).min(1.0);
+        self.a * t.powi(3) + self.b * t.sqr() + self.c * t + self.d
     }
     pub fn get_derivative(&self) -> T {
-        (self.a * 3.0 * self.t.sqr() + self.b * 2.0 * self.t + self.c) * (1.0 / INTERPOLATION_TIME)
+        let t = (self.t / self.interpolation_time).min(1.0);
+        (self.a * 3.0 * t.sqr() + self.b * 2.0 * t + self.c) * (1.0 / self.interpolation_time)
     }
 }
 
@@ -88,10 +101,10 @@ fn test_interpolation() {
     i.server_update(1.0, 1.0);
     assert!(i.get() == 0.0);
     assert!(i.get_derivative() == 1.0);
-    i.update(INTERPOLATION_TIME);
+    i.update(MIN_INTERPOLATION_TIME);
     assert!(i.get() == 1.0);
     assert!(i.get_derivative() == 1.0);
-    i.update(-INTERPOLATION_TIME / 2.0);
+    i.update(-MIN_INTERPOLATION_TIME / 2.0);
     assert!(i.get() == 0.5);
     assert!(i.get_derivative() == 1.0);
 }
